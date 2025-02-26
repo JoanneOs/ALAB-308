9d15a23510827923bce92135b53da4457d018029
JoanneOs <jowan.awsman@yahoo.com>
2025-02-25 21:54:05 -0600

    Copy the code from the editor

--- intro.js
+++ intro.js
@@ -1,4 +1,4 @@
-// simple greeting
+/*// simple greeting
 console.log("Hello PerScholas my name is Joanne Osman");
 
 //  from Objectives 1-creating variable declarations using both let and const.
@@ -67,7 +67,7 @@ Escape sequence	Unicode code point
 \t	tab (U+0009 CHARACTER TABULATION)
 \b	backspace (U+0008 BACKSPACE)
 \f	form feed (U+000C FORM FEED)*/
-let nullCharacter = "This is a null character: \0";
+/*let nullCharacter = "This is a null character: \0";
 console.log(nullCharacter);
 let singleQuote = 'It\'s a sunny day'; // Using \' to include a single quote inside single quotes
 console.log(singleQuote);
@@ -90,3 +90,54 @@ console.log(formFeed);
 
 //7-Implement escape sequences in strings for special characters.
 let message = "This is a very new line\nthis is a tab\tThis is a quote: \"PerSchols\"";
+
+//Use template literals for string interpolation and multi-line strings:
+//not sure
+*/
+
+//Copy the code from the editor into your own file to use as a starting point. Make sure to commit!
+// The initial numbers that must be verified.
+const n1 = 10;
+const n2 = 15;
+const n3 = 20;
+const n4 = 5;
+
+// Check one: add up to 50
+// This is a fairly simple operation using
+// arithmetic operators and a comparison.
+const isSum50 = (n1 + n2 + n3 + n4) == 50;
+
+// Check two: at least two odd numbers
+// Here, we use modulus to check if something is odd.
+// Since % 2 is 0 if even and 1 if odd, we can use
+// arithmetic to count the total number of odd numbers.
+const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+
+// Check three: no number larger than 25
+// This time, we use the OR operator to check
+// if ANY of the numbers is larger than 25.
+const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+
+// Check four: all unique numbers
+// This is long, and there are more efficient
+// ways of handling it with other data structures
+// that we will review later.
+const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+
+// Here, we put the results into a single variable 
+// for convenience. Note how we negate isOver25 using
+// the ! operator. We could also have tested for 
+// "isUnder25" as an alternative.
+const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+
+// Finally, log the results.
+console.log(isValid);
+
+// Here's another example of how this COULD be done,
+// but it SHOULD NOT be done this way. As programmers,
+// we break things into small, manageable pieces so that
+// they can be better understood, scaled, and maintained.
+const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
+  ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
+  !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
+  (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
