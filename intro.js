62de1457d7db1453218f2f5399ce165f03ee4229
JoanneOs <jowan.awsman@yahoo.com>
2025-02-25 21:10:14 -0600

    comparing friends names

--- intro.js
+++ intro.js
@@ -2,8 +2,8 @@
 console.log("Hello PerScholas my name is Joanne Osman");
 
 //  from Objectives 1-creating variable declarations using both let and const.
-let myAge= 39;
-const name= Joanne;
+let myAge = 39;
+const name = Joanne;
 
 // will try to 2 apply different types of data literals.
 //from the lesson: Any value that is not a variable is a literal. They are fixed values that you literally provide to your script.
@@ -13,7 +13,7 @@ console.log(42);
 
 //template literal from the lesson
 const frost =
-	`Nature’s first green is gold,
+    `Nature’s first green is gold,
 Her hardest hue to hold.
 Her early leaf’s a flower;
 But only so an hour.
@@ -34,11 +34,19 @@ console.log(newNumber); // this will type the last value in newNumber
 
 //4-Use comparison operators to compare data.
 // this example for my kids
-let apples=5;
-let oranges=3;
+let apples = 5;
+let oranges = 3;
 //is the number of apples same as orages?
-let isAppleorOrange= apples==oranges;
+let isAppleorOrange = apples == oranges;
 //is the number of apples greater than oranges?
-let isMorethan= apple>oranges;
+let isMorethan = apple > oranges;
 //are oranges less or equal to apples?
-let isLessorSame= oranges <= apples;
+let isLessorSame = oranges <= apples;
+console.log(isAppleorOrange);
+
+//still 4 comparing strings
+let friend1="River";
+let friend2="Selena";
+//are both same?
+let areFreindsSame=friend1==friend2;
+
