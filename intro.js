515846c292d9dde1b067b404dd74e8e5b6b78338
JoanneOs <jowan.awsman@yahoo.com>
2025-02-25 20:41:12 -0600

    let,const

--- intro.js
+++ intro.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+// simple greeting
+console.log("Hello PerScholas my name is Joanne Osman");
+
+//  from Objectives 1-creating variable declarations using both let and const.
+let myAge= 39;
+const name= Joanne;
+
+// will try to 2 apply different types of data literals.
