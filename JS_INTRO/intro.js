9ce5e311cfa431b8c143f5600f98565d289a4c50
JoanneOs <jowan.awsman@yahoo.com>
2025-02-25 20:06:38 -0600

    creating js

---
+++ JS_INTRO/intro.js
@@ -0,0 +1,114 @@
+ let t1=7
+
+ let g// declare the variable
+
+ g="this is my value" //assigning the variable a value
+
+ var myVariable = "some value";
+ let mySecond = true;
+ const PI = 3.1415926
+
+ //an is short for Account Number n, you have to explain or give your variable as meaningfule.
+ let an = 5;
+
+ //or  like this
+ let accountNumber = 5;
+let g=6;
+let r=7;
+ console.log(g+r)
+
+
+let my_account_number = 7
+var myName = "Tishana"
+ //declaring a variable
+myName = "Joy"
+ //re-assigning the same
+
+// see the value of X is deffernet between {} cairly braices
+
+
+
+ let x=10;
+
+ {
+    let x=2;
+}
+
+
+//It is not re-assignable.
+const pi;
+pi=3.14;// will not work
+
+
+//Literals:  Any value that is not a variable is a literal
+console.log(true);
+console.log("Hello World!");
+console.log(42);
+
+//They are fixed values that you literally provide to your script.
+//Literally is for Literals
+
+//type of in termioanl show the type of value
+
+let newNumber = 9;
+newNumber = newNumber + 1;
+
+let newNumber = 9;
+newNumber += 1;
+
+let newNumber = 9;
+newNumber -= 3; // newNumber is now equal to 6
+newNumber /= 3; // newNumber is now equal to 2
+newNumber *= 3; // newNumber is now equal to 6
+newNumber %= 3; // newNumber is now equal to 0
+
+let newNumber = 9;
+newNumber++; // newNumber is now equal to 10
+let newNumber = 9;
+newNumber--; // newNumber is now equal to 8
+
+let newNumber = 9;
+let currentNumber = newNumber++;
+
+let newNumber = 9;
+let currentNumber = newNumber++;
+// newNumber is now equal to 10, but currentNumber is equal to 9
+
+let newNumber = 9;
+let currentNumber = ++newNumber;
+// newNumber and currentNumber are now equal to 10
+
+let isEqual = 3 == 3; // isEqual is true
+let isEqual = 3 == “3”; // isEqual is true
+let isEqual = “3” == “3”; // isEqual is true
+
+let thisLogic = 3 > 2 && 6 == 6; // thisLogic is true
+let thisLogic = 3 > 2 && 6 == 7; // thisLogic is false
+
+let thisLogic = 3 > 2 || 6 == 6; // thisLogic is true
+let thisLogic = 3 > 2 || 6 == 7; // thisLogic is true
+
+let thisLogic = !(3 > 2 && 6 == 6); // thisLogic is false
+let thisLogic = !(3 > 2 || 6 == 7); // thisLogic is false
+
+console.log(“Hello” + “ World”); // outputs: Hello World
+console.log(“This is” + “ a String”); // outputs: This is a String
+console.log(“The number is ” + 5); // outputs: The number is 5
+console.log(“Hello” - “ World”); // Invalid, cannot use -
+
+const name = "Inigo Montoya";
+
+console.log(name[2]); // i
+console.log("Hello World"[4]); // o
+
+const frost =
+	"Nature’s first green is gold, " +
+	"Her hardest hue to hold. " +
+	"Her early leaf’s a flower; " +
+	"But only so an hour. " +
+	"Then leaf subsides to leaf. " +
+	"So Eden sank to grief, " +
+	"So dawn goes down to day. " +
+	"Nothing gold can stay. ";
+
+console.log(frost)
