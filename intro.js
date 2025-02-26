8c156caac252dc88ae41de7b07ddff45242326c9
JoanneOs <jowan.awsman@yahoo.com>
2025-02-25 20:51:49 -0600

    template literal

--- intro.js
+++ intro.js
@@ -6,3 +6,20 @@ let myAge= 39;
 const name= Joanne;
 
 // will try to 2 apply different types of data literals.
+//from the lesson: Any value that is not a variable is a literal. They are fixed values that you literally provide to your script.
+console.log(true);
+console.log("Hello World! again");
+console.log(42);
+
+//template literal from the lesson
+const frost =
+	`Nature’s first green is gold,
+Her hardest hue to hold.
+Her early leaf’s a flower;
+But only so an hour.
+Then leaf subsides to leaf.
+So Eden sank to grief,
+So dawn goes down to day.
+Nothing gold can stay.`;
+
+console.log(frost);
